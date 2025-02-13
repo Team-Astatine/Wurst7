455dadfe57b2215efb70b5b2707911ff6670c42c
JwonLEE <114869036+JAXPLE@users.noreply.github.com>
2025-02-13 15:47:14 +0900

    Edit: Mace 높이계산 식 추가 및 최대값에만 착동하도록 변경

--- src/main/java/net/wurstclient/hacks/MaceDmgHack.java
+++ src/main/java/net/wurstclient/hacks/MaceDmgHack.java
@@ -7,14 +7,20 @@
  */
 package net.wurstclient.hacks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.PositionAndOnGround;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.PlayerAttacksEntityListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.util.ChatUtils;
 
 @SearchTags({"mace dmg", "MaceDamage", "mace damage"})
 public final class MaceDmgHack extends Hack
@@ -47,7 +53,14 @@ public final class MaceDmgHack extends Hack
 		
 		if(!MC.player.getMainHandStack().isOf(Items.MACE))
 			return;
-			
+		
+		int availableHeight = getMaximumHeight(MC.player);
+		if(availableHeight < 25)
+		{
+			ChatUtils.message(availableHeight + "만큼 높이가 부족합니다.");
+			return;
+		}
+		
 		// See ServerPlayNetworkHandler.onPlayerMove()
 		// for why it's using these numbers.
 		// Also, let me know if you find a way to bypass that check in 1.21.
@@ -55,6 +68,29 @@ public final class MaceDmgHack extends Hack
 			sendFakeY(0);
 		sendFakeY(Math.sqrt(500));
 		sendFakeY(0);
+	}
+	
+	private int getMaximumHeight(ClientPlayerEntity player)
+	{
+		int MAX_HEIGHT = 500;
+		int maxAvailableHeight = 0;
+		
+		for(int i = (int)player.getY(); i < MAX_HEIGHT; i++)
+		{
+			BlockPos blockPos =
+				BlockPos.ofFloored(player.getX(), i, player.getZ());
+			Block block = MinecraftClient.getInstance().world
+				.getBlockState(blockPos).getBlock();
+			
+			if(block == Blocks.AIR || block == Blocks.VOID_AIR
+				|| block == Blocks.WATER || block == Blocks.LAVA)
+				maxAvailableHeight++;
+			
+			else
+				break;
+		}
+		
+		return maxAvailableHeight;
 	}
 	
 	private void sendFakeY(double offset)
