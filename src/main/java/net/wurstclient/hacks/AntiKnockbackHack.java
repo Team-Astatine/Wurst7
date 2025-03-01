6ad028013c38b71f51cb76b9c4c3b7928dd736b5
JwonLEE <114869036+JAXPLE@users.noreply.github.com>
2025-03-01 14:53:17 +0900

    Edit: Fix AntiKnockback

--- src/main/java/net/wurstclient/hacks/AntiKnockbackHack.java
+++ src/main/java/net/wurstclient/hacks/AntiKnockbackHack.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2014-2025 Wurst-Imperium and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- */
 package net.wurstclient.hacks;
 
 import net.wurstclient.Category;
@@ -15,23 +8,23 @@ import net.wurstclient.settings.SliderSetting;
 import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"anti knockback", "AntiVelocity", "anti velocity", "NoKnockback",
-	"no knockback", "AntiKB", "anti kb"})
+		"no knockback", "AntiKB", "anti kb"})
 public final class AntiKnockbackHack extends Hack implements KnockbackListener
 {
 	private final SliderSetting hStrength =
-		new SliderSetting("Horizontal Strength",
-			"How far to reduce horizontal knockback.\n"
-				+ "-100% = double knockback\n" + "0% = normal knockback\n"
-				+ "100% = no knockback\n" + ">100% = reverse knockback",
-			1, -1, 2, 0.01, ValueDisplay.PERCENTAGE);
-	
+			new SliderSetting("Horizontal Strength",
+					"수평 방향 넉백 감소 정도.\n"
+							+ "0% = 정상 넉백\n"
+							+ "100% = 넉백 없음",
+					1, 0, 1, 0.01, ValueDisplay.PERCENTAGE);
+
 	private final SliderSetting vStrength =
-		new SliderSetting("Vertical Strength",
-			"How far to reduce vertical knockback.\n"
-				+ "-100% = double knockback\n" + "0% = normal knockback\n"
-				+ "100% = no knockback\n" + ">100% = reverse knockback",
-			1, -1, 2, 0.01, ValueDisplay.PERCENTAGE);
-	
+			new SliderSetting("Vertical Strength",
+					"수직 방향 넉백 감소 정도.\n"
+							+ "0% = 정상 넉백\n"
+							+ "100% = 넉백 없음",
+					1, 0, 1, 0.01, ValueDisplay.PERCENTAGE);
+
 	public AntiKnockbackHack()
 	{
 		super("AntiKnockback");
@@ -39,25 +32,25 @@ public final class AntiKnockbackHack extends Hack implements KnockbackListener
 		addSetting(hStrength);
 		addSetting(vStrength);
 	}
-	
+
 	@Override
 	protected void onEnable()
 	{
 		EVENTS.add(KnockbackListener.class, this);
 	}
-	
+
 	@Override
 	protected void onDisable()
 	{
 		EVENTS.remove(KnockbackListener.class, this);
 	}
-	
+
 	@Override
 	public void onKnockback(KnockbackEvent event)
 	{
 		double verticalMultiplier = 1 - vStrength.getValue();
 		double horizontalMultiplier = 1 - hStrength.getValue();
-		
+
 		event.setX(event.getDefaultX() * horizontalMultiplier);
 		event.setY(event.getDefaultY() * verticalMultiplier);
 		event.setZ(event.getDefaultZ() * horizontalMultiplier);
