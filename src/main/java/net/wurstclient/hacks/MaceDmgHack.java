9ca974ed090d85edf23a260a28eb73416f417685
JwonLEE <114869036+JAXPLE@users.noreply.github.com>
2025-03-02 16:45:49 +0900

    Edit: 공중으로 가끔 순간이동하는 문제 1차 수정 및 코드 반복문으로 수정

--- src/main/java/net/wurstclient/hacks/MaceDmgHack.java
+++ src/main/java/net/wurstclient/hacks/MaceDmgHack.java
@@ -48,10 +48,13 @@ public final class MaceDmgHack extends Hack
 		if(!MC.player.getMainHandStack().isOf(Items.MACE))
 			return;
 		
-		for(int i = 0; i < 4; i++)
-			sendFakeY(0);
-		sendFakeY(Math.sqrt(300));
-		sendFakeY(0);
+		for(int i = 0; i < 7; i++)
+		{
+			int packetOffset = 0;
+			if(i == 4)
+				packetOffset = 300;
+			sendFakeY(packetOffset);
+		}
 	}
 	
 	private int getMaximumHeight(ClientPlayerEntity player)
