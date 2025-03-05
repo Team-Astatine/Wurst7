93e51d900909b1a6849badff9cc2072986568212
jaxple <jaxple@gmail.com>
2025-03-05 13:25:53 +0900

    Add: 공간에 따른 메이스 데미지 변화작업 수열, ignore 블록 추가예정

--- src/main/java/net/wurstclient/hacks/MaceDmgHack.java
+++ src/main/java/net/wurstclient/hacks/MaceDmgHack.java
@@ -19,21 +19,70 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.PlayerAttacksEntityListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
 import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.util.ChatUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SearchTags({"mace dmg", "MaceDamage", "mace damage"})
 public final class MaceDmgHack extends Hack
 	implements PlayerAttacksEntityListener
 {
 	private final SliderSetting sqrtValue = new SliderSetting("Mace Sqrt Value",
-		"description.wurst.setting.maceDmg.SqrtValue", 300, 1, 300, 1,
+		"description.wurst.setting.maceDmg.SqrtValue", 300, 1, 500, 1,
 		SliderSetting.ValueDisplay.DECIMAL);
+	
+	private final CheckboxSetting debuggingButton = new CheckboxSetting(
+		"Debugging", "description.wurst.setting.MaceDmg.Debugging", false);
+	
+	private List<Block> ignoreBlocks = new ArrayList<>();
+	private int[] sqrtValues = {0, 0, 0, 1, 4, 10, 17, 27, 38, 51, 67, 84, 104,
+		125, 148, 174, 201, 231, 262, 295, 331, 368, 408, 449, 492};
+	/*
+	 * 1, 0
+	 * 2, 0
+	 * 3, 1
+	 * 4, 4
+	 * 5, 10
+	 * 6, 17
+	 * 7, 27
+	 * 8, 38
+	 * 9, 51
+	 * 10, 67
+	 * 11, 84
+	 * 12, 104
+	 * 13, 125
+	 * 14, 148
+	 * 15, 174
+	 * 16, 201
+	 * 17, 231
+	 * 18, 262
+	 * 19, 295
+	 * 20, 331
+	 * 21, 368
+	 * 22, 408
+	 * 23, 449
+	 * 24, 492
+	 */
 	
 	public MaceDmgHack()
 	{
 		super("MaceDMG");
 		setCategory(Category.COMBAT);
+		
 		addSetting(sqrtValue);
+		addSetting(debuggingButton);
+		
+		ignoreBlocks.add(Blocks.AIR);
+		ignoreBlocks.add(Blocks.WATER);
+		ignoreBlocks.add(Blocks.TALL_GRASS);
+		ignoreBlocks.add(Blocks.COBWEB);
+		ignoreBlocks.add(Blocks.VOID_AIR);
+		ignoreBlocks.add(Blocks.LAVA);
+		ignoreBlocks.add(Blocks.GLASS);
+		ignoreBlocks.add(Blocks.SHORT_GRASS);
 	}
 	
 	@Override
@@ -54,6 +103,20 @@ public final class MaceDmgHack extends Hack
 		if(!MC.player.getMainHandStack().isOf(Items.MACE))
 			return;
 		
+		int higher = getMaximumHeight(MC.player);
+		if(debuggingButton.isChecked())
+		{
+			ChatUtils.message(String.valueOf(higher));
+			ChatUtils.message(String.valueOf(sqrtValue.getValue()));
+		}
+		
+		if(higher < 1)
+			return;
+		else if(higher > 24)
+			sqrtValue.setValue(500);
+		else
+			sqrtValue.setValue(sqrtValues[higher]);
+		
 		for(int i = 0; i < 4; i++)
 			sendFakeY(0);
 		sendFakeY(Math.sqrt(sqrtValue.getValue()));
@@ -72,10 +135,7 @@ public final class MaceDmgHack extends Hack
 			Block block = MinecraftClient.getInstance().world
 				.getBlockState(blockPos).getBlock();
 			
-			if(block == Blocks.AIR || block == Blocks.VOID_AIR
-				|| block == Blocks.WATER || block == Blocks.LAVA
-				|| block == Blocks.TALL_GRASS || block == Blocks.GLASS
-				|| block == Blocks.COBWEB || block == Blocks.SHORT_GRASS)
+			if(ignoreBlocks.contains(block))
 				maxAvailableHeight++;
 			
 			else
