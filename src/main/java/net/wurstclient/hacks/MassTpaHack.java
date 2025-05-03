8dc4581c1113618cf4aeda47aecfe2aaa065db02
JwonLEE <jaxple@gmail.com>
2025-05-03 13:49:06 +0900

    Refactor MassTpaHack.java for improved readability and structure

--- src/main/java/net/wurstclient/hacks/MassTpaHack.java
+++ src/main/java/net/wurstclient/hacks/MassTpaHack.java
@@ -29,41 +29,44 @@ import net.wurstclient.util.ChatUtils;
 @SearchTags({"mass tpa"})
 @DontSaveState
 public final class MassTpaHack extends Hack
-	implements UpdateListener, ChatInputListener
+		implements UpdateListener, ChatInputListener
 {
 	private static final Pattern ALLOWED_COMMANDS =
-		Pattern.compile("^/+[a-zA-Z0-9_\\-]+$");
-	
+			Pattern.compile("^/+[a-zA-Z0-9_\\-]+$");
+
 	private final TextFieldSetting commandSetting =
-		new TextFieldSetting("Command",
-			"The command to use for teleporting.\n"
-				+ "Examples: /tp, /tpa, /tpahere, /tpo",
-			"/tpa",
-			s -> s.length() < 64 && ALLOWED_COMMANDS.matcher(s).matches());
-	
+			new TextFieldSetting("Command",
+					"The command to use for teleporting.\n"
+							+ "Examples: /tp, /tpa, /tpahere, /tpo",
+					"/tpa",
+					s -> s.length() < 64 && ALLOWED_COMMANDS.matcher(s).matches());
+
 	private final SliderSetting delay = new SliderSetting("Delay",
-		"The delay between each teleportation request.", 20, 1, 200, 1,
-		ValueDisplay.INTEGER.withSuffix(" ticks").withLabel(1, "1 tick"));
-	
+			"The delay between each teleportation request.", 20, 1, 200, 1,
+			ValueDisplay.INTEGER.withSuffix(" ticks").withLabel(1, "1 tick"));
+
 	private final CheckboxSetting ignoreErrors =
-		new CheckboxSetting("Ignore errors",
-			"Whether to ignore messages from the server telling you that the"
-				+ " teleportation command isn't valid or that you don't have"
-				+ " permission to use it.",
-			false);
-	
+			new CheckboxSetting("Ignore errors",
+					"Whether to ignore messages from the server telling you that the"
+							+ " teleportation command isn't valid or that you don't have"
+							+ " permission to use it.",
+					false);
+
 	private final CheckboxSetting stopWhenAccepted = new CheckboxSetting(
-		"Stop when accepted", "Whether to stop sending more teleportation"
+			"Stop when accepted", "Whether to stop sending more teleportation"
 			+ " requests when someone accepts one of them.",
-		true);
-	
+			true);
+
+	private final CheckboxSetting isActiveMassTpaFlooding = new CheckboxSetting(
+			"TPA Flood", "Re-request TPA from all players except my friend", false);
+
 	private final Random random = new Random();
 	private final ArrayList<String> players = new ArrayList<>();
-	
+
 	private String command;
-	private int index;
+	private int sendTpaCount;
 	private int timer;
-	
+
 	public MassTpaHack()
 	{
 		super("MassTPA");
@@ -72,51 +75,56 @@ public final class MassTpaHack extends Hack
 		addSetting(delay);
 		addSetting(ignoreErrors);
 		addSetting(stopWhenAccepted);
+		addSetting(isActiveMassTpaFlooding);
 	}
-	
+
 	@Override
 	protected void onEnable()
 	{
 		// reset state
 		players.clear();
-		index = 0;
+		sendTpaCount = 0;
 		timer = 0;
-		
+
 		// cache command in case the setting is changed mid-run
 		command = commandSetting.getValue().substring(1);
-		
+
 		// collect player names
 		String playerName = MC.getSession().getUsername();
 		for(PlayerListEntry info : MC.player.networkHandler.getPlayerList())
 		{
 			String name = info.getProfile().getName();
 			name = StringHelper.stripTextFormat(name);
-			
+
+			if(isActiveMassTpaFlooding.isChecked()
+					&& WURST.getFriends().contains(name))
+				continue;
+
 			if(name.equalsIgnoreCase(playerName))
 				continue;
-			
+
 			players.add(name);
 		}
-		
+
 		Collections.shuffle(players, random);
-		
+
 		EVENTS.add(ChatInputListener.class, this);
 		EVENTS.add(UpdateListener.class, this);
-		
+
 		if(players.isEmpty())
 		{
 			ChatUtils.error("Couldn't find any players.");
 			setEnabled(false);
 		}
 	}
-	
+
 	@Override
 	protected void onDisable()
 	{
 		EVENTS.remove(ChatInputListener.class, this);
 		EVENTS.remove(UpdateListener.class, this);
 	}
-	
+
 	@Override
 	public void onUpdate()
 	{
@@ -125,46 +133,60 @@ public final class MassTpaHack extends Hack
 			timer--;
 			return;
 		}
-		
-		if(index >= players.size())
+
+		if(isActiveMassTpaFlooding.isChecked()
+				&& sendTpaCount >= players.size())
 		{
+			sendTpaCount = 0;
+
+			if(command.equals("tpa"))
+				command = "tpahere";
+
+			else if(command.equals("tpahere"))
+				command = "tpa";
+		}
+
+		if(!isActiveMassTpaFlooding.isChecked()
+				&& sendTpaCount >= players.size())
+		{
+			command = commandSetting.getValue().substring(1);
 			setEnabled(false);
 			return;
 		}
-		
+
 		MC.getNetworkHandler()
-			.sendChatCommand(command + " " + players.get(index));
-		
-		index++;
+				.sendChatCommand(command + " " + players.get(sendTpaCount));
+
+		sendTpaCount++;
 		timer = delay.getValueI() - 1;
 	}
-	
+
 	@Override
 	public void onReceivedMessage(ChatInputEvent event)
 	{
 		String message = event.getComponent().getString().toLowerCase();
 		if(message.startsWith("\u00a7c[\u00a76wurst\u00a7c]"))
 			return;
-		
+
 		if(message.contains("/help") || message.contains("permission"))
 		{
 			if(ignoreErrors.isChecked())
 				return;
-			
+
 			event.cancel();
 			ChatUtils.error("This server doesn't have a "
-				+ command.toUpperCase() + " command.");
+					+ command.toUpperCase() + " command.");
 			setEnabled(false);
-			
+
 		}else if(message.contains("accepted") && message.contains("request")
-			|| message.contains("akzeptiert") && message.contains("anfrage"))
+				|| message.contains("akzeptiert") && message.contains("anfrage"))
 		{
 			if(!stopWhenAccepted.isChecked())
 				return;
-			
+
 			event.cancel();
 			ChatUtils.message("Someone accepted your " + command.toUpperCase()
-				+ " request. Stopping.");
+					+ " request. Stopping.");
 			setEnabled(false);
 		}
 	}
